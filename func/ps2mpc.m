c5e496001a5f89dfe386b48d3b08ee04fed52c76
cduan2020 <63173470+cduan2020@users.noreply.github.com>
2020-04-06 12:12:35 -0500

    Update ps2mpc.m

--- func/ps2mpc.m
+++ func/ps2mpc.m
@@ -1,32 +1,33 @@
 function mpc=ps2mpc(ps)
 
+% Originnal created by Ferenc Molnar.
 
 % mpc = 
 % 
 %   struct with fields:
 % 
 %     ref_freq: 60
-%      gen_dyn: [3×4 double]
+%      gen_dyn: [3Ã—4 double]
 %      version: '2'
 %      baseMVA: 100
-%          bus: [9×13 double]
-%          gen: [3×21 double]
-%       branch: [9×13 double]
-%      gencost: [3×7 double]
+%          bus: [9Ã—13 double]
+%          gen: [3Ã—21 double]
+%       branch: [9Ã—13 double]
+%      gencost: [3Ã—7 double]
 
 % ps = 
 % 
 %   struct with fields:
 % 
-%          gen: [3×21 double]
-%      gen_dyn: [3×4 double]
-%          bus: [9×13 double]
+%          gen: [3Ã—21 double]
+%      gen_dyn: [3Ã—4 double]
+%          bus: [9Ã—13 double]
 %      baseMVA: 100
-%            Y: [9×9 double]
+%            Y: [9Ã—9 double]
 %     ref_freq: 60
 %      version: '2'
-%       branch: [9×13 double]
-%      gencost: [3×7 double]
+%       branch: [9Ã—13 double]
+%      gencost: [3Ã—7 double]
 
 [branch, Gs, Bs]=Y2Branch(ps.Y);
 mpc=ps;
