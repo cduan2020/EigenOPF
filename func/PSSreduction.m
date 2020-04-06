026632e2dc921d86609dd42a00b57f56425b11f8
cduan2020 <63173470+cduan2020@users.noreply.github.com>
2020-04-06 15:31:51 -0500

    Update PSSreduction.m

--- func/PSSreduction.m
+++ func/PSSreduction.m
@@ -1,6 +1,5 @@
 function [K,T] = PSSreduction(Ka,Tr,Ta,Tb,Tc,Ks,T1,T2,T3,T4)
 
-% sys = zpk([0 -1/T1 -1/T3 -1/Tc],[-1/Tw -1/T2 -1/T4 -1/Ta -1/Tb -1/Td0p],Ks*Ka);
 
 
 sys1 = zpk([-1/T1 -1/T3 -1/Tc],[-1/T2 -1/T4 -1/Ta -1/Tb],Ks*Ka*T1/T2*T3/T4/Ta*Tc/Tb);
